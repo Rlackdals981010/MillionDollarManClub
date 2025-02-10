682de282764d632e9f3fa373b95e9ffd15ae508b
Changmin Kim <ksjchm4@gmail.com>
2025-02-10 21:40:09 +0900

    [feat] 낙관적 락 적용중

--- src/main/java/com/dolloer/million/security/JwtUtil.java
+++ src/main/java/com/dolloer/million/security/JwtUtil.java
@@ -1,19 +1,16 @@
-package com.sparta.doguin.security;
+package com.dolloer.million.security;
 
-import com.sparta.doguin.domain.user.enums.UserRole;
-import com.sparta.doguin.domain.user.enums.UserType;
-import com.sparta.doguin.security.dto.JwtUtilRequest;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import java.rmi.ServerException;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
@@ -25,7 +22,7 @@ public class JwtUtil {
     private static final String BEARER_PREFIX = "Bearer ";
     private static final long TOKEN_TIME = 60 * 60 * 1000L; // 60분
 
-    @Value("${jwt.se  cret.key}")
+    @Value("${jwt.secret.key}")
     private String secretKey;
     private Key key;
     private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
@@ -36,27 +33,24 @@ public class JwtUtil {
         key = Keys.hmacShaKeyFor(bytes);
     }
 
-    public String createToken(JwtUtilRequest.CreateToken createToken) {
+    public String createToken(Long userId, String name) {
         Date date = new Date();
 
-        return Jwts.builder()
-                .setSubject(String.valueOf(createToken.userId()))
-                .claim("email", createToken.email())
-                .claim("nickname", createToken.nickname())
-                .claim("userType", createToken.userType().getUserType())
-                .claim("userRole", createToken.userRole().getUserRole())
-                .setExpiration(new Date(date.getTime() + TOKEN_TIME))
-                .setIssuedAt(date) // 발급일
-                .signWith(key, signatureAlgorithm) // 암호화 알고리즘
-                .compact();
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(String.valueOf(userId))
+                        .claim("name", name)
+                        .setExpiration(new Date(date.getTime() + TOKEN_TIME))
+                        .setIssuedAt(date) // 발급일
+                        .signWith(key, signatureAlgorithm) // 암호화 알고리즘
+                        .compact();
     }
 
-    public String substringToken(String tokenValue) {
+    public String substringToken(String tokenValue) throws ServerException {
         if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
-            return tokenValue.substring(BEARER_PREFIX.length()); // Bearer 접두사 제거
+            return tokenValue.substring(7);
         }
-        log.error("Not Found Token");
-        throw new NullPointerException("Not Found Token");
+        throw new ServerException("Not Found Token");
     }
 
     public Claims extractClaims(String token) {
@@ -65,21 +59,5 @@ public class JwtUtil {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
-    }
-
-    public AuthUser extractAuthUser(String token) {
-        Claims claims = extractClaims(token);
-        Long userId = claims.get("sub", Long.class);
-        String nickname = claims.get("nickname", String.class);
-        String email = claims.get("email", String.class);
-        UserRole role = claims.get("userRole", UserRole.class);
-        UserType type = claims.get("userType", UserType.class);
-        return new AuthUser(userId, email,nickname,type ,role);
-    }
-
-    // 토큰을 헤더에 저장하는 메서드
-    public void addTokenToResponseHeader(String token, HttpServletResponse response) {
-        response.addHeader("Authorization", token);
-        response.setHeader("Access-Control-Expose-Headers", "Authorization");
     }
 }
