c34714d328545608d0cf4571e78047289e033e1c
Changmin Kim <ksjchm4@gmail.com>
2025-02-28 19:17:14 +0900

    [feat] 헬스 체크 api 등록

--- src/main/java/com/dolloer/million/config/SecurityConfig.java
+++ src/main/java/com/dolloer/million/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable) // BasicAuthenticationFilter 비활성화
                 .logout(AbstractHttpConfigurer::disable) // LogoutFilter 비활성화
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/signin", "/signup","/stock/price","/stock/candle").permitAll()
+                        .requestMatchers("/signin", "/signup","/stock/price","/stock/candle","/ping").permitAll()
                         .anyRequest().authenticated()
                 )
                 .build();
