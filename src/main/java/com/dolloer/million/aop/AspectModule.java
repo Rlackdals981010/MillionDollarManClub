8af10a0cc79fa7c137547ca8ca21c54821375721
Changmin Kim <ksjchm4@gmail.com>
2025-03-19 11:03:48 +0900

    [refact] AOP 로그에 사람 이름 출력 추가

--- src/main/java/com/dolloer/million/aop/AspectModule.java
+++ src/main/java/com/dolloer/million/aop/AspectModule.java
@@ -1,11 +1,14 @@
 package com.dolloer.million.aop;
 
+import com.dolloer.million.security.AuthUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -18,15 +21,25 @@ public class AspectModule {
     @Around("@annotation(com.dolloer.million.annotation.LogExecution)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint)throws Throwable {
         String methodName = joinPoint.getSignature().getName();
-        log.info("[{}] 호출", methodName);
+        String username = getCurrentUsername(); // 현재 로그인한 사용자 가져오기
+        log.info("[{}][{}] 호출",username, methodName);
 
         try{
             Object result = joinPoint.proceed();
-            log.info("[{}] 응답", methodName);
+            log.info("[{}][{}] 응답", username,methodName);
             return result;
         }catch (Throwable ex){
             log.error("[{}] 실행중 오류 발생: {}", methodName, ex.getMessage());
             throw ex;
         }
+    }
+
+    // 현재 로그인한 사용자 이름 가져오기
+    private String getCurrentUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof AuthUser) {
+            return ((AuthUser) authentication.getPrincipal()).getName();
+        }
+        return "Anonymous"; // 인증되지 않은 사용자
     }
 }
